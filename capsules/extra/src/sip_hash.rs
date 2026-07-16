48c8036bd504bece3ca848de21a58e47fdee53f3
Andrew Cheung <a7cheung@ucsd.edu>
2026-07-15 17:31:24 -0700

    Reannotation

--- capsules/extra/src/sip_hash.rs
+++ capsules/extra/src/sip_hash.rs
@@ -163,8 +163,9 @@ fn size_of_u16() -> usize {
 
 #[flux_rs::sig(fn(input: &[u8]{n: n >= 2}) -> u16)]
 fn read_le_u16(input: &[u8]) -> u16 {
-    // FLUX-TODO addr=0xa35a flavor=explicit_panic
     let sizeof_u16 = size_of_u16();
+
+    // FLUX-TODO addr=0xa35a flavor=explicit_panic
     flux_support::assert(sizeof_u16 <= input.len());
 
     let (int_bytes, _rest) = input.split_at(sizeof_u16);
